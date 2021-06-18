9f5714351714dcf549df9ecac709da71641f62df
Your Name <you@example.com>
2021-06-18 12:31:38 +0700

    Homework by NDT

---
+++ Ex1/Cooler.cs
@@ -0,0 +1,29 @@
+﻿namespace Ex1
+{
+    public class Cooler
+    {
+        public Cooler(float temperature)
+        {
+            Temperature = temperature;
+        }
+
+        public float Temperature
+        {
+            get { return _Temperature; }
+            set { _Temperature = value; }
+        }
+        private float _Temperature;
+
+        public void OnTemperatureChanged(float newTemperature)
+        {
+            if (newTemperature > Temperature)
+            {
+                System.Console.WriteLine("cooler: on");
+            }
+            else
+            {
+                System.Console.WriteLine("Cooler: off");
+            }
+        }
+    }
+}
---
+++ Ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        public delegate void Increment(ref int X);
+
+        public static void Add2(ref int x)
+        {
+            x += 2;
+        }
+
+        public static void Add3(ref int x)
+        {
+            x += 3;
+        }
+
+        public static void Main(string[] args)
+        {
+            Increment functionDelegate = Add2;
+            functionDelegate += Add3;
+            functionDelegate += Add2;
+            int x = 5;
+            functionDelegate(ref x);
+            Console.ReadLine();
+        }
+    }
+}
